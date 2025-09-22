35e0e3187bbc9257c656053fa8127238497d1d53
wdonova1 <wdonova1@students.kennesaw.edu>
2025-09-22 15:27:03 -0400

    add

---
+++ ASSIGNMENT4/MyStack.java
@@ -0,0 +1,14 @@
+package ASSIGNMENT4;
+
+public class MyStack<E> extends LinkedList_No_HT {
+    private int size=0;
+    public MyStack(){
+        super();
+    }
+    public void push(E data){
+
+    }
+    public void pop(){
+
+    }
+}
